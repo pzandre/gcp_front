5898a1e5712ce6b1040afa4f5d30305b285e3b86
André Perez <andre.perez@dengun.com>
2023-03-22 08:58:21 -0300

    Fix actions

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
+const API_URL = 'https://backend-zch5nxtcla-uc.a.run.app';
 const USERS_API_URL = `${API_URL}/api/users`;
 
 export { API_URL, USERS_API_URL };
